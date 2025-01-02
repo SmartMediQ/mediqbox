ac0128e5cf5bd49a137bdd486a48c572292a04aa
littledumb <jiamin@gmail.com>
2025-01-03 06:25:34 +0800

    add pubmed

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 import os, sys
 
+from dotenv import load_dotenv
+
 sys.path.append(os.path.join(
   os.path.dirname(__file__), os.pardir,
 ))
@@ -9,3 +11,5 @@ sys.dont_write_bytecode = True
 data_dir = os.path.join(
   os.path.dirname(__file__), "data",
 )
+
+load_dotenv()
